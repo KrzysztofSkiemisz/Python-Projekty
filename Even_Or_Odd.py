32173b0547647c7735c79f73b053045e47ac4b45
KrzysztofSkiemisz <111428422+KrzysztofSkiemisz@users.noreply.github.com>
2022-11-22 00:30:32 +0100

    Napisano program sprawdzający parzystość liczby

---
+++ Even_Or_Odd.py
@@ -0,0 +1,11 @@
+# Even or odd number
+
+x = None
+
+while not(x):
+    x = int(input('Enter an int number for x: '))
+
+if x%2== 0:
+    print('Entered number is even!')
+else:
+    print('Entered number is odd!')
